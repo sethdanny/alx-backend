cf1ee30f9c8a23d5117ef102172fe7cfafc23267
sethdanny <naddulidaniel1994@gmail.com>
2023-07-24 01:24:09 +0300

    fixed

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -40,27 +40,27 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-            """Retrieves info about a page from a given index and with a
-        specified size.
-        """
-            data = self.indexed_dataset()
-            assert index is not None and index >= 0 and index <= max(data.keys())
-            page_data = []
-            data_count = 0
-            next_index = None
-            start = index if index else 0
-            for i, item in data.items():
-             if i >= start and data_count < page_size:
+        """Retrieves info about a page from a given index and with a
+    specified size.
+    """
+        data = self.indexed_dataset()
+        assert index is not None and index >= 0 and index <= max(data.keys())
+        page_data = []
+        data_count = 0
+        next_index = None
+        start = index if index else 0
+        for i, item in data.items():
+            if i >= start and data_count < page_size:
                 page_data.append(item)
                 data_count += 1
                 continue
-             if data_count == page_size:
+            if data_count == page_size:
                 next_index = i
                 break
-            page_info = {
+        page_info = {
             'index': index,
             'next_index': next_index,
             'page_size': len(page_data),
             'data': page_data,
         }
-            return page_info
+        return page_info
