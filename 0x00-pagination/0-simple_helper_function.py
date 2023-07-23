9b87f0aad10c83243a134cdf60bbe03bce0f3cdd
sethdanny <naddulidaniel1994@gmail.com>
2023-07-24 00:36:49 +0300

    fixed pycodestyle

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """introduction to pagination"""
 
+
 def index_range(page, page_size):
     """function which retrieves the content on pages"""
     start_index = (page - 1) * page_size
